909c5f260dc207bce84a533a76ea97fe6fd43437
Jack McCallie <65779661+Jack-Mac-21@users.noreply.github.com>
2023-04-09 21:53:11 -0500

    Path change

--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Team } from '../types/Team';
+import { Team } from 'types/Team';
 
 function createTeam(
   name: string,
